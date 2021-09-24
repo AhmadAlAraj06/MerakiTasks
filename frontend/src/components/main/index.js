6850642d5f997bb0b4446c58a4b7d47ce511e6d5
AhmadAlAraj06 <81652458+AhmadAlAraj06@users.noreply.github.com>
2021-09-24 23:53:38 +0300

    a

--- frontend/src/components/main/index.js
+++ frontend/src/components/main/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./main.css";
 import logo from "./aa.png";
 import powerd from "./as.png";
-
+import spring from "./sprin.png";
 const Header = () => {
   return (
     <div className="App">
@@ -154,7 +154,7 @@ const Header = () => {
             <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146zM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5z" />
           </svg>
           <br />
-          
+
           <div className="Search2">Home</div>
         </div>
 
@@ -188,25 +188,111 @@ const Header = () => {
           <div className="cat">Categories</div>
         </div>
 
-        <div className="cart2">
-		<svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-bag" viewBox="0 0 16 16">
-  <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z"/>
-</svg>{" "}
+        <div className="shop">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="26"
+            height="26"
+            fill="currentColor"
+            class="bi bi-bag"
+            viewBox="0 0 16 16"
+          >
+            <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
+          </svg>{" "}
           <br />
           <div className="Search22">Shop</div>
-           
         </div>
 
         <div className="peerson">
-		<svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-gift" viewBox="0 0 16 16">
-  <path d="M3 2.5a2.5 2.5 0 0 1 5 0 2.5 2.5 0 0 1 5 0v.006c0 .07 0 .27-.038.494H15a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1v7.5a1.5 1.5 0 0 1-1.5 1.5h-11A1.5 1.5 0 0 1 1 14.5V7a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h2.038A2.968 2.968 0 0 1 3 2.506V2.5zm1.068.5H7v-.5a1.5 1.5 0 1 0-3 0c0 .085.002.274.045.43a.522.522 0 0 0 .023.07zM9 3h2.932a.56.56 0 0 0 .023-.07c.043-.156.045-.345.045-.43a1.5 1.5 0 0 0-3 0V3zM1 4v2h6V4H1zm8 0v2h6V4H9zm5 3H9v8h4.5a.5.5 0 0 0 .5-.5V7zm-7 8V7H2v7.5a.5.5 0 0 0 .5.5H7z"/>
-</svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="30"
+            height="30"
+            fill="currentColor"
+            class="bi bi-person"
+            viewBox="0 0 16 16"
+          >
+            <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
+          </svg>
           <br />
-          <div className="Search22">Gifts</div>
-
-          
+          <div className="Search222">Account</div>
         </div>
       </div>
+      <br />
+      <br />
+      <br />
+      <br />
+      {/* img title */}
+      <div className="imgt">
+        <img className="imgtt" src={spring} />
+      </div>
+      <br />
+      {/* covid 19 uptade */}
+      <div className="textt">
+        <strong className="strongg"> Covid 19 Update:</strong> We encourage you
+        to shop as normal for all your products but shipping delays may occur.
+        Stay safe NZ!
+      </div>
+      <br />
+
+      {/* list menue */}
+      <div className="list">
+        <h1 className="dis">Discover</h1>
+        <hr />
+        <h4 className="h3h3">Featured Auckland deals</h4>
+        <h4 className="h3h3">Collections</h4>
+        <h4 className="h3h3">Escapes</h4>
+        <h4 className="h3h3">Picked for You</h4>
+        <h4 className="h3h3">Activities, Events & Outdoors</h4>
+        <h4 className="h3h3">Store</h4>
+        <h4 className="h3h3">Restaurants, Bars, Cafes</h4>
+        <h4 className="h3h3">Beauty, Massage & Spa</h4>
+        <h4 className="h3h3">House & Garden</h4>
+        <h4 className="h3h3">Fitness & Sports</h4>
+        <h4 className="h3h3">Automotive</h4>
+      </div>
+
+{/* post 1 */}
+
+
+
+      {/* newsletter*/}
+      <div class="newsletter">
+        <div class="container">
+          <div class="row">
+            <div class="col">
+              <div class="bbb_newsletter_container d-flex flex-lg-row flex-column align-items-lg-center align-items-center justify-content-lg-start justify-content-center">
+                <div>
+                  <h2 className="pnew">
+                    Get the best deals delivered direct to your inbox each day
+                  </h2>
+                </div>
+                <div class="bbb_newsletter_content clearfix">
+                  <form action="#" class="bbb_newsletter_form">
+                    {" "}
+                    <input
+                      type="email"
+                      class="bbb_newsletter_input"
+                      required="required"
+                      placeholder="Enter your email address"
+                    />{" "}
+                    <button class="bbb_newsletter_button">Subscribe</button>{" "}
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
     </div>
   );
 };
